f9849c8e9deb7358333b439124e5e75619c65d56
Andrew Thompson <a.thompson@craneware.com>
2016-02-18 10:54:04 +0000

    Initial

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tokenAuthenticationApp
+ * @description
+ * # tokenAuthenticationApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tokenAuthenticationApp', []);
